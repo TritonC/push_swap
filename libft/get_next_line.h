17d6e6e59b6a803f05926dcf6ea282e1c39c7e59
Manuel Ignacio Luis Fuentes <mluis-fu@c2r9s3.42madrid.com>
2023-03-11 10:53:47 +0100

    finished

--- libft/get_next_line.h
+++ libft/get_next_line.h
@@ -6,21 +6,18 @@
 /*   By: mluis-fu <mluis-fu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 15:19:59 by manuel            #+#    #+#             */
-/*   Updated: 2023/03/11 10:27:37 by mluis-fu         ###   ########.fr       */
+/*   Updated: 2023/03/11 10:53:14 by mluis-fu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 1
-#endif
-
 # include <stdio.h>
 # include <stdlib.h>
 # include <fcntl.h>
 # include <unistd.h>
+
 
 void	*ft_calloc(size_t n, size_t size);
 size_t	ft_strlen(const char *str);
@@ -31,4 +28,8 @@ char	*check_line(char *str);
 char	*rest_of_file(char *buffer);
 char	*read_and_save(int fd, char *buff);
 
+#endif
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 1
 #endif
